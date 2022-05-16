310b40cc3628805adb48cb4d8c70e740eedb69f7
Maykon Oliveira <C0635611@vale.com>
2022-05-16 10:28:34 -0300

    Setup do projeto

---
+++ src/main/kotlin/com/github/maykonoliveira/seeddesafiocdc/SeedDesafioCdcApplication.kt
@@ -0,0 +1,11 @@
+package com.github.maykonoliveira.seeddesafiocdc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeedDesafioCdcApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeedDesafioCdcApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/maykonoliveira/seeddesafiocdc/SeedDesafioCdcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.maykonoliveira.seeddesafiocdc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeedDesafioCdcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
