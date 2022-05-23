c4c376fad28e0da8261e757258b8202e6f16fa95
Maykon Oliveira <C0635611@vale.com>
2022-05-23 12:20:46 -0300

    DESAFIO 1: Exibir a página de detalhe de um livro

--- src/main/kotlin/com/github/maykonoliveira/seeddesafiocdc/api/rest/BookController.kt
+++ src/main/kotlin/com/github/maykonoliveira/seeddesafiocdc/api/rest/BookController.kt
@@ -2,16 +2,18 @@ package com.github.maykonoliveira.seeddesafiocdc.api.rest
 
 import com.github.maykonoliveira.seeddesafiocdc.api.rest.input.BookCreateForm
 import com.github.maykonoliveira.seeddesafiocdc.api.rest.output.BookOnlyIdAndTitle
+import com.github.maykonoliveira.seeddesafiocdc.application.domain.Book
 import com.github.maykonoliveira.seeddesafiocdc.application.repository.AuthorRepository
 import com.github.maykonoliveira.seeddesafiocdc.application.repository.BookRepository
 import com.github.maykonoliveira.seeddesafiocdc.application.repository.CategoryRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 /**
- * CI - 6
+ * CI - 7
  */
 @RestController
 @RequestMapping("/books")
@@ -31,5 +33,12 @@ class BookController(
     @GetMapping
     fun listOnlyIdAndTitle(): List<BookOnlyIdAndTitle> {
         return repository.findAll().map { BookOnlyIdAndTitle(it.id!!, it.title) }
+    }
+
+    @GetMapping("/{id}")
+    fun bookById(@PathVariable id: Long): ResponseEntity<Book> {
+        return repository.findById(id)
+            .map { ResponseEntity.ok(it) }
+            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build())
     }
 }
